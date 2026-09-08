bbd2d6a0735d739a11cb913bfd6dd2659e1ce6ee
Artemelion <artyommorar@gmail.com>
2026-09-08 15:07:24 +0300

    css req correct

--- node_ts_lessons/src/server.ts
+++ node_ts_lessons/src/server.ts
@@ -1,48 +1,48 @@
-import http from "node:http"
-const PORT:number = 4200
-import fs from "node:fs"
-import path from "node:path"
- 
-/*
-https://shop.com/product?name=phone&price=1000
-params
-query params
-body
-*/
-const server = http.createServer((req,res)=>{
-    const PATH_TO_PAGES = path.join("src", "pages")
-    if(req.method==="GET" && req.url === '/')
-    {
-        const PATH_TO_INDEX_PAGE = path.join(PATH_TO_PAGES, "index.html")
-        const content = fs.readFileSync(PATH_TO_INDEX_PAGE)
-        res.setHeader("Content-Type", "text/html; charset=utf-8")
-        res.write(content)
-    }
-    else if(req.method==="GET" && req.url ==='/about')
-    {
-        const PATH_TO_INDEX_PAGE = path.join(PATH_TO_PAGES, "about.html")
-        const content = fs.readFileSync(PATH_TO_INDEX_PAGE)
-        res.setHeader("Content-Type", "text/html; charset=utf-8")
-        res.write(content)
-    }
-    else if(req.method === "POST"){
-        res.setHeader("Content-Type", "application/json; charset=utf-8")
-        const user = {
-            name:"John",
-            age:30
-        }
-        console.log("Request: ", req.url)
-        if(req.url === "/user"){
-        res.write(JSON.stringify(user))
-        }
-    }
-     else if(req.method === "PUT"){
-        res.setHeader("Content-Type", "text/html; charset=utf-8")
-        res.write(`<h1>Ти хочеш оновити дані. Request: ${req.method}</h1>`)
-    }
-   
-    res.end()
-})
-server.listen(PORT,()=>{
-    console.log(`Server http://localhost:${PORT} has been started...`)
-})
+import http from "node:http";
+import fs from "node:fs";
+import path from "node:path";
+
+const PORT: number = 4200;
+
+const PATH_TO_PAGES = path.join("src", "pages");
+
+const server = http.createServer((req, res) => {
+  console.log(req.method, req.url);
+
+  if (req.method === "GET" && req.url === "/") {
+    const filePath = path.join(PATH_TO_PAGES, "index.html");
+    const content = fs.readFileSync(filePath);
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(content);
+    return;
+  }
+
+  if (req.method === "GET" && req.url === "/about") {
+    const filePath = path.join(PATH_TO_PAGES, "about.html");
+    const content = fs.readFileSync(filePath);
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(content);
+    return;
+  }
+
+  if (req.method === "GET" && req.url && path.extname(req.url) === ".css") {
+  const fileName = path.basename(req.url);
+  const filePath = path.join("src", "styles", fileName);
+
+  const content = fs.readFileSync(filePath);
+
+  res.setHeader("Content-Type", "text/css; charset=utf-8");
+  res.end(content);
+  return;
+}
+
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.end("<h1>404 Not Found</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server http://localhost:${PORT} has been started...`);
+});
